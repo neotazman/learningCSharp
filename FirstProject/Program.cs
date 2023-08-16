18a8d7bf0663224360ce5b3dc33f5a5a6a7315f3
Tristan Alie <TristanAlie@yahoo.com>
2023-08-16 18:40:11 -0400

    did some data types and learn about the random class and new keyword

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -5,8 +5,47 @@ string yourName = "dumbass";
 string myName = "Tristan";
 Console.WriteLine("Hello " + yourName + "!"); // basically a console log
 string greeting = $"     Hello {yourName}, I'm {myName}!    "; // "$" goes before the string as opposed to before the "{}", also doesn't need "``" to run
+
 // .Trim is a method that cuts whitespace from the beginning and end of the string -- .TrimStart and .TrimEnd trim only the expected parts
 // C# methods and properties on values are TitleCase
-Console.WriteLine(greeting.Trim()); 
+Console.WriteLine(greeting.Contains("Hello")); 
+greeting = greeting.Trim().Replace("Hello", "Greetings");
+Console.WriteLine(greeting);
+Console.Write("Hello "); // .Write does the same thing as .WriteLine except it doesn't start a new line with each Console
+Console.WriteLine(yourName);
 
 // Console.ReadLine(); // probably was meant for an older version of C#, but it's irrelevant now
+
+// Console.WriteLine("");
+// Console.WriteLine("Floating point types:");
+// Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
+// Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
+// Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+
+// Console.WriteLine("a" == "a ".Trim()); // you can add string methods to strings without saving them as a variable
+
+// string value1 = " a";
+// string value2 = "A ";
+// Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+
+// Random coin = new Random();
+// int flip = coin.Next(0, 2); // returns a value >= the first number and < the second (will never return 2)
+// Console.WriteLine((flip == 0) ? "heads" : "tails");
+
+// string permission = "Admin|Manager";
+// int level = 55;
+
+// if(permission.Contains("Admin")) {
+//     if(level > 55) {
+//         Console.WriteLine("Welcome, Super Admin user.");
+//     } else {
+//         Console.WriteLine("Welcome, Admin user.");
+//     }
+// } // I didn't finish this challenge, i just want to skip to the learning
+
+// var message = "Hello, world"; // var can be set to any data type -- 
+// message = 6; // var implies the first data type set to it, so it can't be reset to a different data type
+
+// int[] data = new int[3]; // instantiating an int array -- note: does not fill the array
+
+// Console.WriteLine(2.625); // The compiler defaults to a double literal when a decimal number is entered without a literal suffix.
--- FirstProject/README.md
+++ FirstProject/README.md
@@ -1,6 +1,7 @@
-#fyi 
+# fyi 
 
     1) won't work when you run program.cs, will only run from LearningC#.csproj -- edit: will run program.cs, but only after you run it from the csproj file, so there's really no point in running program.cs
+        a) typing "dotnet run" into the terminal will always run the program if you're in the right folder
 
     2)
     using System;
@@ -16,3 +17,4 @@
         }
     }   -- you used to have to write this on every file, but .net 6 made it so every file inherently has this coded into the file and the code inside Main is where the code you write is placed
 
+#  The var keyword has an important use in C#. Many times, the type of a variable is obvious from its initialization. In those cases, it's simpler to use the var keyword. The var keyword can also be useful when planning the code for an application. When you begin developing code for a task, you may not immediately know what data type to use. Using var can help you develop your solution more dynamically.
