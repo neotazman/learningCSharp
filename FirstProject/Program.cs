57900fbcafffe5a09d60dc5f439a28f6e0586443
Tristan Alie <TristanAlie@yahoo.com>
2023-08-18 17:00:41 -0400

    learned about arrays, lists, loops and functions

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -1,18 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // need to run LearningC#.csproj to run this code
 
-string yourName = "dumbass";
-string myName = "Tristan";
-Console.WriteLine("Hello " + yourName + "!"); // basically a console log
-string greeting = $"     Hello {yourName}, I'm {myName}!    "; // "$" goes before the string as opposed to before the "{}", also doesn't need "``" to run
+// string yourName = "dumbass";
+// string myName = "Tristan";
+// Console.WriteLine("Hello " + yourName + "!"); // basically a console log
+// string greeting = $"     Hello {yourName}, I'm {myName}!    "; // "$" goes before the string as opposed to before the "{}", also doesn't need "``" to run
 
 // .Trim is a method that cuts whitespace from the beginning and end of the string -- .TrimStart and .TrimEnd trim only the expected parts
 // C# methods and properties on values are TitleCase
-Console.WriteLine(greeting.Contains("Hello")); 
-greeting = greeting.Trim().Replace("Hello", "Greetings");
-Console.WriteLine(greeting);
-Console.Write("Hello "); // .Write does the same thing as .WriteLine except it doesn't start a new line with each Console
-Console.WriteLine(yourName);
+// Console.WriteLine(greeting.Contains("Hello")); 
+// greeting = greeting.Trim().Replace("Hello", "Greetings");
+// Console.WriteLine(greeting);
+// Console.Write("Hello "); // .Write does the same thing as .WriteLine except it doesn't start a new line with each Console
+// Console.WriteLine(yourName);
 
 // Console.ReadLine(); // probably was meant for an older version of C#, but it's irrelevant now
 
@@ -47,5 +47,23 @@ Console.WriteLine(yourName);
 // message = 6; // var implies the first data type set to it, so it can't be reset to a different data type
 
 // int[] data = new int[3]; // instantiating an int array -- note: does not fill the array
+int[] countTo5 = new int[5] {1, 2, 3, 4, 5}; // creating a specific array -- apparently objects and arrays use similar syntax
+
+List<int> intList = new List<int>() {1, 2, 3};
+intList.Add(66);
+
+// LOOPS
+foreach (int number in intList) {
+    Console.WriteLine(number);
+}
+for(int i = 0; i < 10; i++) {
+    Console.WriteLine(i);
+} 
 
 // Console.WriteLine(2.625); // The compiler defaults to a double literal when a decimal number is entered without a literal suffix.
+
+// FUNCTIONS
+void consoleMessage(string message) {
+    Console.WriteLine(message);
+} // functions are created with the return value
+consoleMessage("Hello");
